8c4963861c9b56a5e1bb10842d81899fcd0bafa4
zhangbaobao-94 <56239019+zhangbaobao-94@users.noreply.github.com>
2019-12-01 16:04:49 +0800

    Update Main.java
    添加一行代码

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld2!");
+        System.out.println("HelloWorld3!");
     }
 
 }
